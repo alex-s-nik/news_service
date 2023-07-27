df718e0c40e0319c329c6e554c726383a8bf401f
alex-s-nik <to_ctrl@mail.ru>
2023-07-27 22:31:35 +0300

    fix: del unused files and code

--- my_news/users/admin.py
+++ my_news/users/admin.py
@@ -1,25 +1,8 @@
-from django import forms
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.utils.translation import gettext_lazy as _
 
 from .models import User
-
-
-class CustomUserCreationForm(UserCreationForm):
-    role = forms.ChoiceField(choices=User.ROLE_CHOICES)
-
-    class Meta(UserCreationForm.Meta):
-        model = User
-        fields = UserCreationForm.Meta.fields + ('role',)
-
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.role = self.cleaned_data['role']
-        if commit:
-            user.save()
-        return user
 
 
 class CustomUserAdmin(UserAdmin):
