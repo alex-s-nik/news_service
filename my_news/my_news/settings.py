99c32f560622736683d03d93e08c767e4cde5d95
alex-s-nik <to_ctrl@mail.ru>
2023-07-27 13:32:01 +0300

    fix: rest framework app name

--- my_news/my_news/settings.py
+++ my_news/my_news/settings.py
@@ -19,7 +19,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'restframework',
+    'rest_framework',
 
     'users.apps.UsersConfig',
     'news.apps.NewsConfig',
