29e611ef63056d27b31a095a04e44dc5fa1e7d31
alex-s-nik <to_ctrl@mail.ru>
2023-07-27 16:24:14 +0300

    feat: change su creation set admin role for su when the user is creating

--- my_news/users/models.py
+++ my_news/users/models.py
@@ -1,13 +1,7 @@
+from typing import Iterable, Optional
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-USER_ROLE = 'user'
-ADMIN_ROLE = 'admin'
-
-ROLE_CHOICES = (
-    (USER_ROLE, 'Пользователь'),
-    (ADMIN_ROLE, 'Администратор')
-)
 
 
 class User(AbstractUser):
@@ -16,6 +10,14 @@ class User(AbstractUser):
     Пользователи могут удалять и изменять свои новости. Админ может удалять и изменять любую новость.
     Лайкать и комментировать может любой пользователь, автор может удалять комментарии к своим новостям, админ может удалять любые комментарии.
     """
+    USER_ROLE = 'user'
+    ADMIN_ROLE = 'admin'
+
+    ROLE_CHOICES = (
+        (USER_ROLE, 'Пользователь'),
+        (ADMIN_ROLE, 'Администратор')
+    )
+
     role = models.CharField(
         verbose_name='Роль пользователя',
         max_length=20,
@@ -23,6 +25,11 @@ class User(AbstractUser):
         default=USER_ROLE
     )
 
+    def save(self, *args, **kwargs) -> None:
+        if self.is_superuser:
+            self.role = User.ADMIN_ROLE
+        return super().save(*args, **kwargs)
+
     @property
     def is_admin(self):
-        return self.is_superuser or self.role == ADMIN_ROLE
+        return self.is_superuser or self.role == User.ADMIN_ROLE
