b464d5ca7b264c0172b0f4f10f94b11e6eaa1e32
alex-s-nik <to_ctrl@mail.ru>
2023-07-31 18:47:04 +0300

    refactor: user test

--- my_news/tests/test_role.py
+++ my_news/tests/test_role.py
@@ -1,36 +1,29 @@
 import pytest
 
+from users.factories import AdminUserFactory, UserFactory
 from users.models import User
 
 class TestModelRole:
-    @pytest.mark.parametrize('method,data,expected_role', [
+    @pytest.mark.parametrize('user_factory, data, expected_role', [
         (
-            User.objects.create_user,
-            {
-                'username': 'test_username',
-                'email': 'test_mail@fake.com',
-                'password': 'test_pass'
-            },
+            UserFactory,
+            {},
             User.USER_ROLE
         ),
         (
-            User.objects.create_user,
-            {
-                'username': 'test_username',
-                'email': 'test_mail@fake.com',
-                'password': 'test_pass',
-                'role': User.ADMIN_ROLE
-            },
+            UserFactory,
+            {'role': User.ADMIN_ROLE},
             User.ADMIN_ROLE
         ),
-        (User.objects.create_superuser, {
-            'username': 'test_username',
-            'email': 'test_mail@fake.com',
-            'password': 'test_pass'
-        }, User.ADMIN_ROLE)
-    ])
+        (
+            AdminUserFactory,
+            {},
+            User.ADMIN_ROLE
+        )
+    ]
+            
+    )
     @pytest.mark.django_db()
-    def test_role_while_created(self, method, data, expected_role):
-        
-        user = method(**data)
+    def test_role(self, user_factory,data, expected_role):
+        user = user_factory.create(**data)
         assert user.role == expected_role
