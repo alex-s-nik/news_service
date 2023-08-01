aa5795d3e46a723d3fed5ee3c13fb65088ca0add
alex-s-nik <to_ctrl@mail.ru>
2023-08-01 10:30:15 +0300

    feat: add postresql support

--- my_news/my_news/settings.py
+++ my_news/my_news/settings.py
@@ -66,8 +66,12 @@ WSGI_APPLICATION = 'my_news.wsgi.application'
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.sqlite3'),
-        'NAME': os.getenv('DB_NAME', BASE_DIR / 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME' 'postgres'),
+        'USER': os.getenv('DB_USER', 'postgres'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'postgres'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
