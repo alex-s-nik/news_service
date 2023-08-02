b83b132e8b9fd5115c7791a760904f2f857247b4
alex-s-nik <to_ctrl@mail.ru>
2023-08-02 10:42:31 +0300

    refactor: change const for last comments Change const name COMMENTS_COUNT to LAST_COMMENTS_COUNT and change scope for import it to tests

--- my_news/api/serializers.py
+++ my_news/api/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 
 from news.models import Comment, News
 
+LAST_COMMENTS_COUNT = 10
+
 
 class CommentSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(read_only=True, slug_field='username')
@@ -23,10 +25,9 @@ class NewsSerializer(serializers.ModelSerializer):
         return obj.comments.count()
     
     def get_last_comments(self, obj):
-        COMMENTS_COUNT = 10
 
         return CommentSerializer(
-            obj.comments.order_by('-created_at')[:COMMENTS_COUNT],
+            obj.comments.order_by('-created_at')[:LAST_COMMENTS_COUNT],
             many=True
         ).data
 
